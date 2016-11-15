219dbf5faf6980a55583a0116c50838ea9c5da7c
Graham Lee <wgin.lee@gmail.com>
2016-11-15 23:11:34 +0000

    char counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var test = $('.new-tweet');
+  $('.new-tweet').on('input', 'textarea', function() {
+      var charsLeft = 140 - this.value.length;
+      var counter = $(this).siblings('.counter');
+      counter.text(charsLeft);
+      if (charsLeft < 0){
+        counter.addClass('char-exceeded');
+      } else {
+        counter.removeClass('char-exceeded');
+      }
+  });
+});
